efc1af988c693789285c036df90b00b27f8640f6
Bárbara Cáceres S <39339093+Barby87@users.noreply.github.com>
2021-07-28 18:51:22 -0400

    Feature: add routes to containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import './App.css';
 import MyFair from './containers/myFair/MyFair';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import About from './containers/about/About';
+import Home from './containers/home/Home';
+import NotFound from './containers/notFound.tsx/NotFound';
 
 function App() {
   return (
-    <MyFair/>
+   <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Home/>
+          </Route>
+          <Route path="/about" exact>
+            <About/>
+          </Route>
+          <Route path="/myFair" exact>
+            <MyFair/>
+          </Route>
+          <Route path="*" exact>
+            <NotFound/>
+          </Route>
+        </Switch>
+      </Router>
+   </div>
   );
 }
 
